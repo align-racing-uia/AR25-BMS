d5811daee655ad0a98ed7c58442c90a6a1be9b08
Håvard Nygård <haavardn@uia.no>
2025-01-27 21:52:12 +0100

    write almost implemented

--- Core/Inc/bq79600.h
+++ Core/Inc/bq79600.h
@@ -1,7 +1,14 @@
 #ifndef BQ79600_H
 #define BQ79600_H
 #include "main.h"
+#include "stdbool.h"
 
+#define BQ_TIMEOUT 2000
+
+// BQ79600 Own ID
+#define BQ_SELF_ID 0x00
+
+// Initialization Bytes
 #define BQ_DEVICE_READ 0x80
 #define BQ_DEVICE_WRITE 0x90
 #define BQ_STACK_READ 0xA0
@@ -10,11 +17,20 @@
 #define BQ_BROAD_WRITE 0xD0
 #define BQ_BROAD_REVERSE_WRITE 0xE0
 
+// Some important registers
+#define BQ_CONTROL1 0x0309
+
+#define BQ_CONTROL1_SEND_WAKE 1<<5
+
 typedef struct {
 
     SPI_HandleTypeDef* hspi;
     GPIO_TypeDef* csGPIOx;
+    GPIO_TypeDef* spiRdyGPIOx;
+    GPIO_TypeDef* mosiGPIOx;
     uint16_t csPin;
+    uint16_t spiRdyPin;
+    uint16_t mosiPin;
 
 
 } BQ_HandleTypeDef;
@@ -22,6 +38,12 @@ typedef struct {
 void BQ_Init(BQ_HandleTypeDef* hbq);
 
 void BQ_Wake(BQ_HandleTypeDef* hbq);
+
+bool BQ_SpiRdy(BQ_HandleTypeDef* hbq);
+
+uint8_t BQ_Read(BQ_HandleTypeDef* hbq, uint8_t *dataOut, uint8_t deviceId, uint16_t regAddr, uint8_t dataLength, uint8_t readType);
+
+uint8_t BQ_Write(BQ_HandleTypeDef* hbq, uint8_t *inData, uint8_t deviceId, uint16_t regAddr, uint8_t dataLength, uint8_t writeType);
 
 
 #endif
--- Core/Src/bq79600.c
+++ Core/Src/bq79600.c
@@ -1,5 +1,6 @@
 #include "bq79600.h"
 #include "align-utils.h"
+#include "spi.h"
 
 void BQ_Init(BQ_HandleTypeDef* hbq){
     HAL_GPIO_WritePin(hbq->csGPIOx, hbq->csPin, GPIO_PIN_SET);
@@ -7,6 +8,81 @@ void BQ_Init(BQ_HandleTypeDef* hbq){
 
 void BQ_Wake(BQ_HandleTypeDef* hbq){
     HAL_GPIO_WritePin(hbq->csGPIOx, hbq->csPin, GPIO_PIN_RESET);
-    Align_DelayUs(10);
-    HAL_GPIO_WritePin(hbq->csGPIOx, hbq->csPin, GPIO_PIN_SET);
+    Align_DelayUs(2); // Atleast 2 us
+    HAL_GPIO_WritePin(hbq->mosiGPIOx, hbq->mosiPin, GPIO_PIN_RESET);
+    HAL_Delay(3); // Atleast 2.75 ms
+    HAL_GPIO_WritePin(hbq->mosiGPIOx, hbq->mosiPin, GPIO_PIN_SET);
+    Align_DelayUs(2); // Atleast 2 us
+    HAL_GPIO_writePin(hbq->csGPIOx, hbq->csPin, GPIO_PIN_SET);
+    HAL_Delay(4); // Atleast 3.75 us
+    uint8_t data[1] = {BQ_CONTROL1_SEND_WAKE}; // Data to send to CONTROL1[SEND_WAKE]
+    BQ_Write(hbq, &data, BQ_SELF_ID, BQ_CONTROL1, 1, BQ_DEVICE_WRITE);
+
+    // Wait 1.6ms + 10ms pr device on bus
+    // 10x 1.6+10 = 116ms
+    
+    HAL_Delay(120);
+}
+
+bool BQ_SpiRdy(BQ_HandleTypeDef* hbq){
+    return HAL_GPIO_ReadPin(hbq->spiRdyGPIOx, hbq->spiRdyPin) == GPIO_PIN_SET;
+}
+
+// Output:
+// 0 - Everything is ok
+// 1 - SPI not ready
+uint8_t BQ_Read(BQ_HandleTypeDef* hbq, uint8_t *dataOut, uint8_t deviceId, uint16_t regAddr, uint8_t dataLength, uint8_t readType){
+    if(BQ_SpiRdy(hbq) != true){
+        return 1;
+    }
+    HAL_GPIO_WritePin(hbq->csGPIOx, hbq->csPin, GPIO_PIN_RESET);
+    Align_DelayUs(1);
+
+    return 0;
+}
+
+// Output:
+// 0 - Everything is ok
+// 1 - SPI not ready
+// 2 - Invalid writeType
+// 3 - Too much data, over 28 bytes
+uint8_t BQ_Write(BQ_HandleTypeDef* hbq, uint8_t *inData, uint8_t deviceId, uint16_t regAddr, uint8_t dataLength, uint8_t writeType){
+    if(BQ_SpiRdy(hbq) != true){
+        return 1;
+    }
+    if((writeType != BQ_DEVICE_WRITE) && (writeType != BQ_DEVICE_WRITE) && (writeType != BQ_DEVICE_WRITE)){
+        return 2;
+    }
+    if(dataLength > 28){
+        return 3;
+    }
+    // To limit the program size for now
+    uint8_t writeData[32] = {0};
+
+    uint8_t writeSize = 0;
+    writeData[writeSize] = writeType;
+    writeSize++;
+
+    if(writeType == BQ_DEVICE_WRITE){
+        writeData[1] = deviceId;
+        writeSize++;
+    }
+    writeData[writeSize] = (uint8_t) (regAddr << 8);
+    writeSize++;
+    writeData[writeSize] = (uint8_t) regAddr;
+    writeSize++;
+
+    for(int i=0; i<dataLength; i++){
+        writeData[writeSize] = inData[i];
+        writeSize++;
+    }  
+
+    // Whole transmit should be ready here
+
+    HAL_GPIO_WritePin(hbq->csGPIOx, hbq->csPin, GPIO_PIN_RESET);
+    Align_DelayUs(1); // Atleast 50ns, we dont have that kind of resolution
+
+    HAL_SPI_Transmit(hbq->hspi, &writeData, writeSize, BQ_TIMEOUT);
+
+    return 0;
 }
