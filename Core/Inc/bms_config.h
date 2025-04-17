5c07df8544b39a7e8b6dfa9e6f2bd1fe0b22ec47
hrrs01 <haavard.hn@gmail.com>
2025-04-17 22:24:57 +0200

    Added gate, to make development without test rig easier

--- Core/Inc/bms_config.h
+++ Core/Inc/bms_config.h
@@ -95,6 +95,6 @@ typedef enum
 
 BMS_StatusTypeDef BMS_Config_WriteToFlash(BMS_ConfigTypeDef *bms_config, uint8_t *eeprom, size_t size);
 BMS_StatusTypeDef BMS_Config_UpdateFromFlash(BMS_ConfigTypeDef *bms_config, uint8_t *eeprom, size_t size);
-
+BMS_StatusTypeDef BMS_Config_HandleCanMessage(BMS_ConfigTypeDef *bms_config, uint16_t packet_id, uint8_t *data);
 
 #endif // __BMS_CONFIG_H
---
+++ Core/Src/bms_config.c
@@ -0,0 +1,11 @@
+#include "bms_config.h"
+
+BMS_StatusTypeDef BMS_Config_WriteToFlash(BMS_ConfigTypeDef *bms_config, uint8_t *eeprom, size_t size){
+
+}
+BMS_StatusTypeDef BMS_Config_UpdateFromFlash(BMS_ConfigTypeDef *bms_config, uint8_t *eeprom, size_t size){
+
+}
+BMS_StatusTypeDef BMS_Config_HandleCanMessage(BMS_ConfigTypeDef *bms_config, uint16_t packet_id, uint8_t *data){
+
+}
