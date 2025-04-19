c577fcb6975f387ab53621f8e8d1a0e2d7ffc37d
hrrs01 <haavard.hn@gmail.com>
2025-04-19 20:13:52 +0200

    Added check to see if its the correct DMA transfer

--- Core/Src/secondary_mcu.c
+++ Core/Src/secondary_mcu.c
@@ -4,5 +4,8 @@ uint8_t secondary_mcu_recieve_index = 1; // Flag to check if the secondary MCU i
 // Start at 1 to wrap to 0 the first run
 
 void SecondaryMCU_RecieveCallback(DMA_HandleTypeDef *hdma) {
-    secondary_mcu_recieve_index = !secondary_mcu_recieve_index; // Alternate the index
+    if(hdma->ChannelIndex == 6) { // Check if the DMA is for the SPI1 RX
+        secondary_mcu_recieve_index = !secondary_mcu_recieve_index; // Alternate the index
+
+    }
 }
