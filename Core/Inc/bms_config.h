8b1b7c4ebefc4d23056291240bc66e53ef7f85ff
hrrs01 <haavard.hn@gmail.com>
2025-06-19 19:15:38 +0200

    Added sensible default values based on current setup

--- Core/Inc/bms_config.h
+++ Core/Inc/bms_config.h
@@ -5,53 +5,6 @@
 #include "stddef.h"
 #include "stdbool.h"
 
-#define BMS_CONFIG_VERSION 2 // This number will automatically increment when the config changes
-
-// These are absolute maxes for the battery model, not the actual values
-// The actual values are set in the battery model init function
-
-#define TEMP_MAP_POOL_MAX_POINTS 100 // Size of the OCV map pool, defaults to a maximum of 5 temperature maps with 15 points each
-#define TEMP_MAP_POOL_AMOUNT 5       // Size of the temperature map pool, defaults to a maximum of 5 temperature maps with 15 points each
-
-#define BQ_MAX_AMOUNT_OF_CHIPS 15                            // The maximum amount of chips in the system
-#define BQ_MAX_AMOUNT_OF_SLAVES (BQ_MAX_AMOUNT_OF_CHIPS - 1) // The maximum amount of BQ79616 chips in the system
-#define BQ_MAX_AMOUNT_OF_CELLS_EACH 16                       // The maximum amount of cells in series on each board
-#define BQ_MAX_AMOUNT_OF_TEMPS_EACH 14                       // The maximum amount of temperature sensors on each board
-
-#define CELL_MEMORY_POOL_SIZE (BQ_MAX_AMOUNT_OF_SLAVES * BQ_MAX_AMOUNT_OF_CELLS_EACH) // Size of the cell memory pool, defaults to a maximum of 300 cells
-
-// Default values for compiled programs can be set in this header
-// These values are only used if the EEPROM is empty, corrupt, or not present
-#define DEFAULT_TOTAL_CHIPS 11                         // Including master
-#define DEFAULT_TOTAL_SLAVES (DEFAULT_TOTAL_CHIPS - 1) // Number of slaves in the system
-#define DEFAULT_CELLS_EACH 14                          // Number of cells in series on each slave
-#define DEFAULT_TEMPS_EACH 6                           // Number of temperature sensors on each slave
-#define DEFAULT_TEMP_MAP_VOLTAGE_POINTS 5              // Number of voltage points in each temperature map
-#define DEFAULT_TEMP_MAP_AMOUNT 3                      // Number of temperature maps
-#define DEFAULT_CELLS_IN_PARALLEL 1                    // Number of cells in parallel
-#define DEFAULT_CELLVOLTAGE_LIMIT_LOW 2000             // mV
-#define DEFAULT_CELLVOLTAGE_LIMIT_HIGH 4200            // mV
-#define DEFAULT_CELLTEMPERATURE_LIMIT_LOW 0            // C
-#define DEFAULT_CELLTEMPERATURE_LIMIT_HIGH 60          // C
-#define DEFAULT_CAN_NODE_ID 0x06
-#define DEFAULT_CAN_CONFIG_NODE_ID (DEFAULT_CAN_NODE_ID + 1) // The CAN ID of the BMS, this is used to identify the BMS on the CAN bus
-#define DEFAULT_CAN_BROADCAST_PACKET 0x01
-#define DEFAULT_CAN_BAUDRATE (uint16_t)500000 // 500kbit/s
-#define DEFAULT_CAN_EXTENDED false            // Should the CAN ID be extended or not
-#define DEFAULT_BROADCAST_PACKET 0x01
-#define DEFAULT_USB_LOGGING_ENABLED 0               // Should the USB logging be enabled or not
-#define DEFAULT_USB_LOGGING_INTERVAL 500            // The interval for the USB logging
-#define DEFAULT_CAN_BROADCAST_INTERVAL 100          // The interval for the CAN broadcast
-#define DEFAULT_CAN_TEMP_BROADCAST_INTERVAL 1000    // The interval for the CAN temperature broadcast
-#define DEFAULT_CAN_CHARGER_BROADCAST_INTERVAL 1000 // The interval for the CAN charger broadcast
-#define DEFAULT_CAN_CHARGER_BROADCAST_TIMEOUT 5000  // The timeout for the charger broadcast packet
-#define DEFAULT_BALANCE_WHILE_CHARGING false        // Should the BMS balance while charging or not
-
-#define DEFAULT_MULTIPLEX_ENABLED true // If the temperature sensors are multiplexed
-#define DEFAULT_MULTIPLEX_PIN_INDEX 7  // The pin used to multiplex the temperature sensors
-
-#define DEFAULT_TOTAL_CELLS (DEFAULT_TOTAL_SLAVES * DEFAULT_CELLS_EACH * DEFAULT_CELLS_IN_PARALLEL)
-#define DEFAULT_TOTAL_CELLS_IN_SERIES (DEFAULT_TOTAL_SLAVES * DEFAULT_CELLS_EACH)
 
 typedef enum
 {
@@ -87,8 +40,6 @@ typedef struct
     uint16_t SingleCellCapacity;             // The capacity of a single cell in mAh
 
     uint16_t FuseCurrentLimit; // The fuse current limit for the BMS, this is the maximum current that can flow through the fuse
-    uint16_t CCWarningLimit;   // The charging current limit for the BMS during warning state (No AMS fault, only open AIRs)
-    uint16_t DCWarningLimit;   // The discharge current limit for the BMS during warning state (No AMS fault, only open AIRs)
 
     uint8_t CanNodeID;                    // This follows the CAN ID format specified by DTI
     BMS_Config_CanSpeedTypeDef CanSpeed;  // The baudrate of the CAN bus
@@ -156,5 +107,7 @@ void BMS_Config_SetParameter(BMS_Config_HandleTypeDef *bms_config, BMS_Config_Pa
 BMS_Config_StatusTypeDef BMS_Config_WriteToFlash(BMS_Config_HandleTypeDef *bms_config);
 BMS_Config_StatusTypeDef BMS_Config_UpdateFromFlash(BMS_Config_HandleTypeDef *bms_config);
 BMS_Config_StatusTypeDef BMS_Config_HandleCanMessage(BMS_Config_HandleTypeDef *bms_config, uint16_t packet_id, uint8_t *can_data);
+
+
 
 #endif // __BMS_CONFIG_H
--- Core/Src/bms_config.c
+++ Core/Src/bms_config.c
@@ -1,40 +1,92 @@
 #include "bms_config.h"
 #include "w25q_mem.h"
 
+// ====== BMS CONFIG DEFAULTS ======
+#define BMS_CONFIG_VERSION 0
+#define BMS_CONFIG_DEFAULT_NUM_OF_SLAVES 10
+#define BMS_CONFIG_DEFAULT_CELLS_EACH 14
+#define BMS_CONFIG_DEFAULT_TEMPS_EACH 6
+#define BMS_CONFIG_DEFAULT_FIRST_TEMP_PIN_INDEX 1
+#define BMS_CONFIG_DEFAULT_MULTIPLEX_PIN_INDEX 7
+#define BMS_CONFIG_DEFAULT_MULTIPLEX_ENABLED true
+
+#define BMS_CONFIG_DEFAULT_CELLS_IN_PARALLEL 5
+#define BMS_CONFIG_DEFAULT_CELL_VOLTAGE_LIMIT_LOW 2700
+#define BMS_CONFIG_DEFAULT_CELL_VOLTAGE_LIMIT_HIGH 4200
+#define BMS_CONFIG_DEFAULT_CELL_VOLTAGE_DERATE_LIMIT_LOW 3000
+#define BMS_CONFIG_DEFAULT_CELL_VOLTAGE_DERATE_LIMIT_HIGH 4100
+#define BMS_CONFIG_DEFAULT_CELL_TEMPERATURE_LIMIT_LOW -20.0f
+#define BMS_CONFIG_DEFAULT_CELL_TEMPERATURE_LIMIT_HIGH 60.0f
+#define BMS_CONFIG_DEFAULT_CELL_TEMPERATURE_DERATE_LIMIT_LOW 0.0f
+#define BMS_CONFIG_DEFAULT_CELL_TEMPERATURE_DERATE_LIMIT_HIGH 50.0f
+#define BMS_CONFIG_DEFAULT_CELL_DISCHARGE_CURRENT_LIMIT 40000 // 4000 mA = 4 A
+#define BMS_CONFIG_DEFAULT_CELL_CHARGE_CURRENT_LIMIT 2800 // 2800 mA = 2.8 A
+#define BMS_CONFIG_DEFAULT_SINGLE_CELL_CAPACITY 0
+
+#define BMS_CONFIG_DEFAULT_FUSE_CURRENT_LIMIT 200000 // 200000 mA = 200 A, this is the fuse current limit for the BMS, this is the maximum current that can flow through the fuse continuously
+
+#define BMS_CONFIG_DEFAULT_CAN_NODE_ID 6
+#define BMS_CONFIG_DEFAULT_CAN_SPEED BMS_Config_CAN_SPEED_500K // Default CAN speed is 500k, this is the baudrate of the CAN bus
+#define BMS_CONFIG_DEFAULT_BROADCAST_PACKET_ID 1
+#define BMS_CONFIG_DEFAULT_CAN_BROADCAST_INTERVAL 10
+#define BMS_CONFIG_DEFAULT_CAN_TEMP_BROADCAST_INTERVAL 10
+#define BMS_CONFIG_DEFAULT_CAN_VOLTAGE_BROADCAST_INTERVAL 10
+#define BMS_CONFIG_DEFAULT_CAN_CHARGER_BROADCAST_INTERVAL 100
+#define BMS_CONFIG_DEFAULT_CAN_CHARGER_BROADCAST_TIMEOUT 2000
+#define BMS_CONFIG_DEFAULT_CAN_EXTENDED false
 
 void BMS_Config_Init(BMS_Config_HandleTypeDef *bms_config)
 {
-    // Setting the default values of the config
+    if (bms_config == NULL)
+    {
+        return;
+    }
+
     bms_config->ConfigVersion = BMS_CONFIG_VERSION;
+    bms_config->NumOfSlaves = BMS_CONFIG_DEFAULT_NUM_OF_SLAVES;
+    bms_config->CellsEach = BMS_CONFIG_DEFAULT_CELLS_EACH;
+    bms_config->TempsEach = BMS_CONFIG_DEFAULT_TEMPS_EACH;
+    bms_config->FirstTempPinIndex = BMS_CONFIG_DEFAULT_FIRST_TEMP_PIN_INDEX;
+    bms_config->MultiplexPinIndex = BMS_CONFIG_DEFAULT_MULTIPLEX_PIN_INDEX;
+    bms_config->MultiplexEnabled = BMS_CONFIG_DEFAULT_MULTIPLEX_ENABLED;
+
+    bms_config->CellsInParallel = BMS_CONFIG_DEFAULT_CELLS_IN_PARALLEL;
+    bms_config->CellVoltageLimitLow = BMS_CONFIG_DEFAULT_CELL_VOLTAGE_LIMIT_LOW;
+    bms_config->CellVoltageLimitHigh = BMS_CONFIG_DEFAULT_CELL_VOLTAGE_LIMIT_HIGH;
+    bms_config->CellVoltageDerateLimitLow = BMS_CONFIG_DEFAULT_CELL_VOLTAGE_DERATE_LIMIT_LOW;
+    bms_config->CellVoltageDerateLimitHigh = BMS_CONFIG_DEFAULT_CELL_VOLTAGE_DERATE_LIMIT_HIGH;
+    bms_config->CellTemperatureLimitLow = BMS_CONFIG_DEFAULT_CELL_TEMPERATURE_LIMIT_LOW;
+    bms_config->CellTemperatureLimitHigh = BMS_CONFIG_DEFAULT_CELL_TEMPERATURE_LIMIT_HIGH;
+    bms_config->CellTemperatureDerateLimitLow = BMS_CONFIG_DEFAULT_CELL_TEMPERATURE_DERATE_LIMIT_LOW;
+    bms_config->CellTemperatureDerateLimitHigh = BMS_CONFIG_DEFAULT_CELL_TEMPERATURE_DERATE_LIMIT_HIGH;
+    bms_config->CellDischargeCurrentLimit = BMS_CONFIG_DEFAULT_CELL_DISCHARGE_CURRENT_LIMIT;
+    bms_config->CellChargeCurrentLimit = BMS_CONFIG_DEFAULT_CELL_CHARGE_CURRENT_LIMIT;
+    bms_config->SingleCellCapacity = BMS_CONFIG_DEFAULT_SINGLE_CELL_CAPACITY;
+
+    bms_config->FuseCurrentLimit = BMS_CONFIG_DEFAULT_FUSE_CURRENT_LIMIT;
+
+    bms_config->CanNodeID = BMS_CONFIG_DEFAULT_CAN_NODE_ID;
+    bms_config->CanSpeed = BMS_CONFIG_DEFAULT_CAN_SPEED;
+    bms_config->BroadcastPacketID = BMS_CONFIG_DEFAULT_BROADCAST_PACKET_ID;
+    bms_config->CanBroadcastInterval = BMS_CONFIG_DEFAULT_CAN_BROADCAST_INTERVAL;
+    bms_config->CanTempBroadcastInterval = BMS_CONFIG_DEFAULT_CAN_TEMP_BROADCAST_INTERVAL;
+    bms_config->CanVoltageBroadcastInterval = BMS_CONFIG_DEFAULT_CAN_VOLTAGE_BROADCAST_INTERVAL;
+    bms_config->CanChargerBroadcastInterval = BMS_CONFIG_DEFAULT_CAN_CHARGER_BROADCAST_INTERVAL;
+    bms_config->CanChargerBroadcastTimeout = BMS_CONFIG_DEFAULT_CAN_CHARGER_BROADCAST_TIMEOUT;
+    bms_config->CanExtended = BMS_CONFIG_DEFAULT_CAN_EXTENDED;
+
     bms_config->MemoryCheck[0] = 'a';
     bms_config->MemoryCheck[1] = 'l';
     bms_config->MemoryCheck[2] = 'i';
     bms_config->MemoryCheck[3] = 'g';
     bms_config->MemoryCheck[4] = 'n';
-
-    bms_config->NumOfSlaves = DEFAULT_TOTAL_CHIPS - 1; // The master is not counted as a slave
-    bms_config->CellsEach = DEFAULT_CELLS_EACH;
-    bms_config->TempsEach = DEFAULT_TEMPS_EACH;
-    bms_config->CellVoltageLimitLow = DEFAULT_CELLVOLTAGE_LIMIT_LOW;
-    bms_config->CellVoltageLimitHigh = DEFAULT_CELLVOLTAGE_LIMIT_HIGH;
-    bms_config->CellTemperatureLimitLow = DEFAULT_CELLTEMPERATURE_LIMIT_LOW;   // -40C
-    bms_config->CellTemperatureLimitHigh = DEFAULT_CELLTEMPERATURE_LIMIT_HIGH; // 85C
-    bms_config->CanNodeID = DEFAULT_CAN_NODE_ID;
-    bms_config->CanExtended = DEFAULT_CAN_EXTENDED; // Should the CAN ID be extended or not
-    bms_config->CanBroadcastInterval = DEFAULT_CAN_BROADCAST_INTERVAL;                // 100ms
-    bms_config->CanTempBroadcastInterval = DEFAULT_CAN_TEMP_BROADCAST_INTERVAL;       // 1s
-    bms_config->CanVoltageBroadcastInterval = DEFAULT_CAN_TEMP_BROADCAST_INTERVAL;    // 1s
-    bms_config->CanChargerBroadcastInterval = DEFAULT_CAN_CHARGER_BROADCAST_INTERVAL; // 1s
-    bms_config->CanChargerBroadcastTimeout = DEFAULT_CAN_CHARGER_BROADCAST_TIMEOUT;   // 5s
-    bms_config->Checksum = 0x00;                                                      // TODO: Implement CRC checksum
+    bms_config->Checksum = 0;
 }
-
 
 // Set a parameter in the configuration, index is the parameter index, value is the value to set
 // TODO: Implement more parameters
 void BMS_Config_SetParameter(BMS_Config_HandleTypeDef *bms_config, uint8_t index, uint16_t value)
 {
-
 }
 
 BMS_Config_StatusTypeDef BMS_Config_WriteToFlash(BMS_Config_HandleTypeDef *bms_config)
@@ -100,11 +152,9 @@ BMS_Config_StatusTypeDef BMS_Config_UpdateFromFlash(BMS_Config_HandleTypeDef *bm
         return BMS_CONFIG_INVALID_VALUE;
     }
 
-    // TODO: Implement CRC checksum
-
     return BMS_CONFIG_OK;
 }
+
 BMS_Config_StatusTypeDef BMS_Config_HandleCanMessage(BMS_Config_HandleTypeDef *bms_config, uint16_t packet_id, uint8_t *can_data)
 {
-    
 }
